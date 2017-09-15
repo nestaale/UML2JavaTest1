1a5769c6163b56761c8298434a22d1a391a399cf
nestaale <nestaale@gmail.com>
2017-09-15 14:46:55 +0200

    Add files via upload

---
+++ dst/WordCount.java
@@ -0,0 +1,14 @@
+   public class WordCount {
+     private  word;
+
+     private  count;
+ 
+     public  getWord() {
+       return this.word;
+     }
+
+     public  getCount() {
+       return this.count;
+     }
+ 
+   }
