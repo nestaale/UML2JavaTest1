00a8efbea1b825489e917347dfdbfe950e6c7023
nestaale <nestaale@gmail.com>
2017-09-18 11:26:11 +0200

    Add files via upload

---
+++ dst/LineSplitter.java
@@ -0,0 +1,14 @@
+public class LineSplitter implements FlatMapFunction<String, > {
+
+  @Override
+  public void flatMap (String value, Collector<> out) {
+    String[] tokens = value.toLowerCase().split("\\W+");
+
+for (String token : tokens) {
+    if (token.length() > 0) {
+        out.collect(new WordCount(token, 1));
+
+    }
+}
+  }
+}
--- dst/WordCount.java
+++ dst/WordCount.java
@@ -1,14 +1,38 @@
-   public class WordCount {
-     private  word;
 
-     private  count;
- 
-     public  getWord() {
-       return this.word;
-     }
 
-     public  getCount() {
-       return this.count;
-     }
+public class WordCount {
+    private  word;
+
+    private  count;
  
-   }
+    public WordCount() {
+
+    }
+
+    public WordCount( word, count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public  getword() {
+        return word;
+    }
+
+    public void setword( word) {
+        this.word = word;
+    }
+
+    public  getcount() {
+        return count;
+    }
+
+    public void setcount( count) {
+        this.count = count;
+    }
+ 
+    @Override
+    public String toString() {
+        return "(" + this.word + this.count + ")";
+    }
+
+}
